fb5cd0c7e4f58ae55d5d2d23ffe20eec609cb2c7
sannaengstrom <sannaengstrom97@hotmail.com>
2025-04-08 09:43:12 +0200

    la till ReviewController med routing och DI

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -3,3 +3,15 @@
 //Ta bort review
 //Redigera review
 //Hämta alla reviews på en specifik bok
+
+[Route("api/reviews")]
+[ApiController]
+public class ReviewController : ControllerBase
+{
+    private readonly IReviewService _reviewService;
+
+    public ReviewController(IReviewService reviewService)
+    {
+        _reviewService = reviewService;
+    }
+}
