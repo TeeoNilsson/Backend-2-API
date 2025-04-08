bc84470f2f14ad311548ff166f5d4ead5f2b65ab
sannaengstrom <sannaengstrom97@hotmail.com>
2025-04-08 14:00:55 +0200

    Controller review ny branch

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -1,5 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
 //Lägga till en review
 //Like/Dislike
 //Ta bort review
 //Redigera review
-//Hämta alla reviews på en specifik bok
+
+namespace Backend_2_API.Controllers
+{
+    [Route("api/reviews")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        //Hämta alla reviews på en specifik bok
+        [HttpGet("books/{bookId}/reviews")]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReviewsForBook(Guid bookId)
+        {
+
+            var result = await _reviewService.GetReviewsByBookIdAsync(bookId);
+
+            return Ok(result);
+        }
+    }
+}
