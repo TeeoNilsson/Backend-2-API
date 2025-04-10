f91122c0a42653ffaf2e6dc4a68ee5e523fcea62
lovisa-syl <lovisa.sylvan@gmail.com>
2025-04-10 09:11:52 +0200

    Ändrat lite i Interfaces för reviews

--- Interface/IReviewRepository.cs
+++ Interface/IReviewRepository.cs
@@ -1,8 +1,8 @@
 public interface IReviewRepository
 {
     Task<IEnumerable<Review>> GetBookByIdAsync(Guid bookId); // Hämta recnsioner till en bok
-    Task<Review> AddAsync(Review review); // Lägg till recension
+    Task AddReviewAsync(Review review); // Lägg till recension
     Task<bool> UpdateAsync(Review review); // Redigera recension
-    Task<bool> DeleteAsync(Guid reviewId); // Ta bort recension
+    Task<int> DeleteAsync(Guid reviewId); // Ta bort recension
     Task<bool> LikeAsync(Guid reviewId); // Gilla recension
 }
