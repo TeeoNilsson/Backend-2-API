7b85abdfce7cd62276f02e134128d67d5315a934
TeoNilsson <teo.nilsson99@gmail.com>
2025-04-15 09:54:45 +0200

    Removed BookDto from controller

--- DTOs/Reviews/BookDto.cs
+++ DTOs/Reviews/BookDto.cs
@@ -1,9 +1,11 @@
-public class BookDto
+public class BookDto()
 {
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public List<Review> Reviews { get; set; }
     public string Author { get; set; }
     public int Likes { get; set; }
-    public List<ReviewDto> Reviews { get; set; } 
+    public string UserId { get; set; }
+
 }
